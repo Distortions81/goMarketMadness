a94123341ca84aef43af9ecc662074dabd612593
Distortions81 <carlotto81@gmail.com>
2024-09-29 19:44:56 -0600

    remove title

--- banking.go
+++ banking.go
@@ -1,6 +1,3 @@
-/***************
-* STOCK MARKET *
-****************/
 /*
  * Copyright (C) 2024 Carl Frank Otto III
  * All rights reserved.
@@ -255,4 +252,19 @@ func (loan *loanData) makeLoanPayment(amount float64) {
 		loan.Principal = 0
 		loan.Complete = true
 	}
+}
+
+func (player *playerData) credit(income float64) {
+	player.Balance = roundToCent(player.Balance + income)
+}
+
+func (player *playerData) debit(charge float64) bool {
+	newAmount := roundToCent(player.Balance - charge)
+
+	if newAmount <= 0 {
+		player.Bankrupt = true
+		return false
+	}
+	player.Balance = newAmount
+	return true
 }
--- settings.go
+++ settings.go
@@ -1,6 +1,3 @@
-/***************
-* STOCK MARKET *
-****************/
 /*
  * Copyright (C) 2024 Carl Frank Otto III
  * All rights reserved.
